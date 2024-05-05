87ac5769c66b029c07b159b1171fcb03d745e39b
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-05 03:28:00 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+#  get 'lists/new'
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+#  post 'lists' => 'lists#create'
+#  get 'lists' => 'lists#index'
+#  get 'lists/:id' => 'lists#show', as: 'list'
+#  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+#  patch 'lists/:id' => 'lists#update', as: 'update_list'
+#  delete 'listes/:id' => 'lists#destroy', as: 'destroy_list'
+
+  resources :lists
 end
